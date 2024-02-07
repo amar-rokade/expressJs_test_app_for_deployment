ce56a0db2854bbae683b009bcc73ba34dfdcfe28
amar <rokade6117@gmail.com>
2024-02-07 23:30:58 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "NamsteBytes channel" });
+  setTimeout(() => {
+    res.render("index", { title: "NamsteBytes channel" });
+  }, 5000);
 });
 
 module.exports = router;
